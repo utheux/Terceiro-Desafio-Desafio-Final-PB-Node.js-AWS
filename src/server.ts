ec21b83cfdd644708bc869406fe6200bd84fb9f3
Matheus Ribeiro <utheuz_ribeiro@hotmail.com>
2024-10-08 15:46:05 -0300

    feat: add Swagger endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import { AppDataSource } from './database/data-source';
 import routes from './routes/index.routes';
+import swaggerUi from 'swagger-ui-express';
 import 'reflect-metadata';
+import documentation from '../documentation.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(documentation));
 
 app.use(routes);
 
